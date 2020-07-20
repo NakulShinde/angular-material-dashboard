3ce603bd9d6bd6a40043698f49c2fb58b37ec3df
Nakul Shinde <nakulshinde.it@gmail.com>
2020-07-20 22:53:24 +0530

    Adding responsive cards layout to dashboard component

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
+import { MatDividerModule, MatCardModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
     DashboardComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FlexLayoutModule,
+    MatDividerModule,
+    MatCardModule
   ],
   exports: [
     DashboardComponent
